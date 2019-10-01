38975adc13582aeb13c11cabd65279e6775862ea
chaeyoun <chaeyounsong@gmail.com>
2019-10-01 16:17:00 +0900

    studied if condition
    && both true!
    || one true.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const title = document.getElementById("title");
-title.innerHTML = 'Hi! from JS.'
+const age = prompt('How old are you?')
+
+if(age>=18&&age>50){
+    alert(`you can drink but you have to quit.`);
+}else if(age>=18&&age<50){
+    alert(`yes~ enjoy the drunk day!`);
+}else{
+    alert(`too young.`);
+}
